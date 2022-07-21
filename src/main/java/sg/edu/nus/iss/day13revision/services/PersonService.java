0a5ff1a214ade9d23f2877fecdc18b1938701361
MuruGeshhh <108484042+MuruGeshhh@users.noreply.github.com>
2022-07-21 10:48:55 +0800

    service update

---
+++ src/main/java/sg/edu/nus/iss/day13revision/services/PersonService.java
@@ -0,0 +1,35 @@
+package sg.edu.nus.iss.day13revision.services;
+
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+import sg.edu.nus.iss.day13revision.models.*; //import model
+
+//SERVICE
+
+
+@Service
+public class PersonService {
+    private List<Person> persons = new ArrayList<>();
+
+
+    public PersonService() {
+
+        persons.add(new Person("Mark", "zukerberg"));
+        persons.add(new Person("Elon", "Mush"));
+        persons.add(new Person("Donald", "Trump"));
+
+ }
+
+    public List<Person> Persons(){
+        return this.persons;
+
+    
+    }
+    public void addPerson (Person p){
+        persons.add(new Person(p.getFirstName(),p.getLastName()));
+    }
+    
+}
